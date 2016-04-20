8cdb97e08500da5cdec8d820f4a21930df59ef49
StanleyTack <stanley.tack@users.noreply.github.com>
2016-04-20 00:15:28 -0500

    Added logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,18 +8,37 @@ class GameController < ApplicationController
     # ===============================================================
 
     # Your logic here
+    valid_choices = ["rock", "paper", "scissors"]
+    ai_choice = valid_choices.sample
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    if @user_move == "rock" && ai_choice == "paper"
+      result = "lose"
+    elsif @user_move == "rock" && ai_choice == "scissors"
+      result = "win"
+    elsif @user_move == "paper" && ai_choice == "rock"
+      result = "win"
+    elsif @user_move == "paper" && ai_choice == "scissors"
+      result = "lose"
+    elsif @user_move == "scissors" && ai_choice == "rock"
+      result = "lose"
+    elsif @user_move == "scissors" && ai_choice == "paper"
+      result = "win"
+    elsif @user_move == ai_choice
+      result = "tie"
+    end
 
-    @result = "Replace this string with the correct value."
 
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+      # In the end, make sure you assign the correct values to the
+      #   following two variables:
 
-    render("rps.html.erb")
+      @computer_move = "#{ai_choice}"
+
+      @result = "#{result}"
+      # ===============================================================
+      # Your code goes above.
+      # ===============================================================
+
+      render("rps.html.erb")
   end
 end
