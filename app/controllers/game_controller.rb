705c8fde4ac803a1aa472aa9babcb29a373483d0
Bo Gustafsson <bgustafsson2017@>
2016-04-21 17:31:58 -0500

    Done and working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,31 +1,33 @@
 class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
-    @computer_move = ["rock", "paper", "scissors"].sample
+    comp_choice = ["rock", "paper", "scissors"].sample
     # ===============================================================
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
     # Your logic here
-    if @user_move == @computer_move
-      puts "tied!"
-    elsif @user_move == "paper" && @computer_move == "rock"
-      puts = "win!"
-    elsif @user_move == "paper" && @computer_move == "scissors"
-      puts "lose!"
-    elsif @user_move == "rock" && @computer_move == "paper"
-        @result = "lose!"
-    elsif @user_move == "rock" && @computer_move == "scissors"
-      @result = "win!"
-  elsif @user_move == "scissors" && @computer_move == "rock"
-        @result = "lose!"
-    elsif @user_move == "scissors" && @computer_move == "paper"
-      @result = "win!"
     # In the end, make sure you assign the correct values to the
     #   following two variables:
+    if @user_move == comp_choice
+      result = "tie"
+    elsif @user_move == "paper" && comp_choice == "rock"
+      result = "win"
+    elsif @user_move == "paper" && comp_choice == "scissors"
+      result "lose"
+    elsif @user_move == "rock" && comp_choice == "paper"
+        result = "lose"
+    elsif @user_move == "rock" && comp_choice == "scissors"
+      result = "win"
+  elsif @user_move == "scissors" && comp_choice == "rock"
+        result = "lose"
+    elsif @user_move == "scissors" && comp_choice == "paper"
+      result = "win"
+    end
 
-
+@computer_move = "#{comp_choice}"
+@result = "#{result}"
 
     # ===============================================================
     # Your code goes above.
