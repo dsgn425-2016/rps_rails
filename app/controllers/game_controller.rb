7bb8620eca3c146d9c45c21b7dedc65d5f1f5017
Bo Gustafsson <bgustafsson2017@>
2016-04-21 17:09:25 -0500

    Starting out, not working so far

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,20 +1,31 @@
 class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
-
+    @computer_move = ["rock", "paper", "scissors"].sample
     # ===============================================================
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
     # Your logic here
-
+    if @user_move == @computer_move
+      puts "tied!"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      puts = "win!"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      puts "lose!"
+    elsif @user_move == "rock" && @computer_move == "paper"
+        @result = "lose!"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = "win!"
+  elsif @user_move == "scissors" && @computer_move == "rock"
+        @result = "lose!"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result = "win!"
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
 
     # ===============================================================
     # Your code goes above.
