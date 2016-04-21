ab4ab111011c8771c87d51a2578eedb59ef4bb8f
ad1441 <adivakaran2017@kellogg.northwestern.edu>
2016-04-21 12:44:27 -0500

    anil-d-final

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,14 +7,45 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    # Your logic here
+random_move = rand(1..3)
+
+if (random_move == 1)
+  random_move_string = 'rock'
+elsif (random_move == 2)
+  random_move_string = 'paper'
+else
+  random_move_string = 'scissors'
+end
+
+if (random_move_string == @user_move)
+  result_calc = 'tie'
+elsif (random_move_string == 'rock')
+  if (@user_move == 'paper')
+    result_calc = 'win'
+  else
+    result_calc = 'lose'
+  end
+elsif (random_move_string == 'paper')
+  if (@user_move == 'rock')
+    result_calc = 'win'
+  else
+    result_calc = 'lose'
+  end
+else
+  if (@user_move == 'paper')
+    result_calc = 'win'
+  else
+    result_calc = 'lose'
+  end
+end
+
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = random_move_string
 
-    @result = "Replace this string with the correct value."
+    @result = result_calc
 
     # ===============================================================
     # Your code goes above.
