881d0e5cf660f848217df754ba4b71116b323fc7
rphellerman <rphellerman@users.noreply.github.com>
2016-04-22 12:06:22 -0700

    it worked!

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,18 +7,23 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    # Your logic here
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = "Replace this string with the correct value."
-
-    @result = "Replace this string with the correct value."
-
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+    if @computer_move == @user_move
+      @result = " tie!"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result = " win!"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = " lose!"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @result = " win!"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = " lose!"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result = " win!"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result = " lose!"
+    end
 
     render("rps.html.erb")
   end
