807b81f73295edb348d7c839dc9c73cb38acd024
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-04-23 15:18:12 -0500

    Final commit, completed

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,31 +5,20 @@ class GameController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @user_move == @computer_move
-      @result "You tied!"
-     elsif @user_move == "paper" && @computer_move == "rock"
-      @result "You win!"
-     elsif @user_move == "paper" && @computer_move == "scissors"
-      @result "You lose!"
-     elsif @user_move == "scissors" && @computer_move == "rock"
-      @result "You lose!"
-     elsif @user_move == "scissors" && @computer_move == "paper"
-      @result "You win!"
-     elsif @user_move == "rock" && @computer_move == "paper"
-      @result "You lose!"
-     elsif @user_move == "rock" && @computer_move == "scissors"
-      @result "You win!"
-     end
-
-#  if @user_move == @computer_move
-    @result = "It's a tie"
-
-
-
-
-
-
-
-
+      @result = "tied!"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @result = "win!"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = "lose!"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result = "lose!"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result = "win!"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result = "lose!"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = "win!"
+    end
     render("rps.html.erb")
   end
 end
