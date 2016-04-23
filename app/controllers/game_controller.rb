f833f49596ba443fff7c5d6f458fbca77395ab8e
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-04-23 15:13:16 -0500

    First Commit, logic added

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,23 +2,33 @@ class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-    # Your logic here
+    if @user_move == @computer_move
+      @result "You tied!"
+     elsif @user_move == "paper" && @computer_move == "rock"
+      @result "You win!"
+     elsif @user_move == "paper" && @computer_move == "scissors"
+      @result "You lose!"
+     elsif @user_move == "scissors" && @computer_move == "rock"
+      @result "You lose!"
+     elsif @user_move == "scissors" && @computer_move == "paper"
+      @result "You win!"
+     elsif @user_move == "rock" && @computer_move == "paper"
+      @result "You lose!"
+     elsif @user_move == "rock" && @computer_move == "scissors"
+      @result "You win!"
+     end
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
+#  if @user_move == @computer_move
+    @result = "It's a tie"
 
-    @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
 
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+
+
+
+
 
     render("rps.html.erb")
   end
