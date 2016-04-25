8003f36a0a945b1d2231a92d895ca18d82b2fa83
cschafer40 <cschafer2017@users.noreply.github.com>
2016-04-25 07:52:26 -0500

    RPS with rails
    Need to figure out display of computer move

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,28 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @comp_move = ["rock", "paper", "scissors"].sample
 
-    @result = "Replace this string with the correct value."
+    if @user_move == @comp_move
+        @result = "tie"
+      elsif @user_move == "rock" && @comp_move == "paper"
+        @result = "lose"
+      elsif @user_move == "rock" && @comp_move == "scissors"
+        @result = "win"
+      elsif @user_move == "paper" && @comp_move == "scissors"
+        @result = "lose"
+      elsif @user_move == "paper" && @comp_move == "rock"
+        @result = "win"
+      elsif @user_move == "scissors" && @comp_move == "rock"
+        @result = "lose"
+      elsif @user_move == "scissors" && @comp_move == "paper"
+        @result = "win!"
+      else
+        @result = "Try again."
+      end
+
+
+    @result = "#{@result}"
 
     # ===============================================================
     # Your code goes above.
