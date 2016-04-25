def92b56ffbccc7e2d35818345ef5fe603185dd8
vsathpathy <vsathpathy2017@kellogg.northwestern.edu>
2016-04-24 19:45:17 -0500

    RPS Rails with logic
    Did not remove some default text that was appearing on the page "find me
    at <loc>". But logic of RPS works and displays the correct result.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,24 +1,19 @@
 class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
+    @moves=["rock","paper","scissors"]
+    @computer_movesave = @moves.sample
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
+      if @user_move.downcase==@computer_movesave.downcase
+        @resultsave="TIE"
+      elsif (@user_move.downcase=="scissors" && @computer_movesave.downcase=="rock") || (@user_move.downcase=="rock" && @computer_movesave.downcase=="paper")||(@user_move.downcase=="paper" && @computer_movesave.downcase=="scissors")
+      @resultsave= "LOSE"
+      elsif (@user_move.downcase=="scissors" && @computer_movesave.downcase=="paper") ||(@user_move.downcase=="paper" && @computer_movesave.downcase=="rock") ||(@user_move.downcase=="rock" && @computer_movesave.downcase=="scissors")
+      @resultsave="WIN"
+    end
 
-    # Your logic here
-
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = "Replace this string with the correct value."
-
-    @result = "Replace this string with the correct value."
-
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+    @computer_move = "#{@computer_movesave}"
+    @result = "#{@resultsave}"
 
     render("rps.html.erb")
   end
